2c059aee7b50331cb226af173db02bc06aa7d1a1
rosi1 <rosicnaan@gmail.com>
2022-03-17 11:07:05 +0200

    adding project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import culture from '../culture.png';
-import yummy from '../yum.png'
+import yummy from '../yum.png';
+import weather from '../weather.png';
+import travel from '../travel.png';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faSearchPlus} from '@fortawesome/free-solid-svg-icons';
 import {PopupboxManager, PopupboxContainer} from 'react-popupbox';
@@ -14,6 +16,30 @@ const Project = () => {
             <b style={{alignItems: 'center'}}>Leisure Culture</b><br />
             <img className="portfolio-image-popupbox" src={culture} alt='leisure culture clone project'/><br />
             <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/rosi1/Leisure-and-Culture.git")}>https://github.com/rosi1/Leisure-and-Culture.git</a>
+            </>
+        )
+       
+        PopupboxManager.open({content})
+    }
+
+    const openPopupboxTravel = () =>{
+        const content=(
+            <>
+            <b style={{alignItems: 'center'}}>Travel with us</b><br />
+            <img className="portfolio-image-popupbox" src={travel} alt='Travel with us project'/><br />
+            <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/rosi1/travel_with_us.git")}>https://github.com/rosi1/travel_with_us.git</a>
+            </>
+        )
+       
+        PopupboxManager.open({content})
+    }
+
+    const openPopupboxWeather = () =>{
+        const content=(
+            <>
+            <b>Weather in a second</b><br />
+            <img className="portfolio-image-popupbox" src={weather} alt='Weather in a second project'/><br />
+            <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/rosi1/hackaton-2")}>https://github.com/rosi1/hackaton-2</a>
             </>
         )
        
@@ -45,16 +71,31 @@ const Project = () => {
             <div className="container">
                 <h1 className="text-uppercase text-center py-5">Projects</h1>
                 <div className="image-box-wrapper">
-                    <div className="portfolio-image-box" onClick={openPopupboxCulture}>
-                    <img className="portfolio-image" src={culture} alt="portfolio" />
-                    <div className="overflow"></div>
-                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
-                    </div>
+                    
                     <div className="portfolio-image-box" onClick={openPopupboxYummy}>
                     <img className="portfolio-image" src={yummy} alt="portfolio" />
                     <div className="overflow"></div>
                     <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
                     </div>
+
+                    <div className="portfolio-image-box" onClick={openPopupboxTravel}>
+                    <img className="portfolio-image" src={travel} alt="portfolio" />
+                    <div className="overflow"></div>
+                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+                    </div>
+
+                    <div className="portfolio-image-box" onClick={openPopupboxWeather}>
+                    <img className="portfolio-image" src={weather} alt="portfolio" />
+                    <div className="overflow"></div>
+                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+                    </div>
+
+                    <div className="portfolio-image-box" onClick={openPopupboxCulture}>
+                    <img className="portfolio-image" src={culture} alt="portfolio" />
+                    <div className="overflow"></div>
+                    <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus}/>
+                    </div>
+
                 </div>
             <PopupboxContainer {...popupConfigYummy}/>
         </div>
